0b9329125dd3515d4a6d0d5f0e3a8916a21b458c
Kenneth Fung <kenneth0603@gmail.com>
2023-12-05 06:26:51 -0500

    Add express-rate-limit for DDoS protection

--- crypto-wallet-backend/index.js
+++ crypto-wallet-backend/index.js
@@ -2,15 +2,30 @@ const express = require("express");
 const Moralis = require("moralis").default;
 const app = express();
 const cors = require("cors");
+const rateLimit = require('express-rate-limit')
 require("dotenv").config();
 const helmet = require('helmet')
 const port = 7771;
 
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000,
+  max: 20,
+  message: 'Too many requests, please try again later!'
+})
+
+app.set('trust proxy', 1);
 app.use(cors());
 app.use(express.json());
 app.use(helmet())
 app.disable('x-powered-by')
 
+
+app.use("/test", limiter);
+app.get("/test", async (req, res) => {
+  res.send('hello world')
+})
+
+app.use("/getTokens", limiter);
 app.get("/getTokens", async (req, res) => {
   try{
     const { userAddress, chain } = req.query;
@@ -33,7 +48,6 @@ app.get("/getTokens", async (req, res) => {
     const jsonResponse = {
       tokens: tokens.raw,
       transactions: transaction.result,
-      //nfts: myNfts,
       balance: balance.raw.balance / (10 ** 18)
     }
   
